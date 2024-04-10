e7e1cf4aec910066da425badb060f4f01afb0461
manana <manana@host.docker.internal>
2024-04-10 13:46:05 +0200

    Primer commit

---
+++ 01_herencia_puntos_jm/src/principal/test.java
@@ -0,0 +1,13 @@
+package principal;
+
+import service.Punto3D;
+
+public class test {
+
+	public static void main(String[] args) {
+		Punto3D p3d =new Punto3D(25);
+		p3d.imprimir();
+
+	}
+
+}
---
+++ 01_herencia_puntos_jm/src/service/Punto3D.java
@@ -0,0 +1,19 @@
+package service;
+
+public class Punto3D extends Punto{
+	private int z;
+	
+	public Punto3D (int z) {
+		super();  // Implicita!!!!. Llamada al contructor sin parámetros de la superclase
+		this.z = 0;
+	}
+
+	public int getZ() {
+		return z;
+	}
+
+	public void setZ(int z) {
+		this.z = z;
+	}
+	
+}
