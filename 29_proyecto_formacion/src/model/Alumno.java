0dfe223cccf174ed757faefdad9f0096918c2292
manana <manana@host.docker.internal>
2024-04-24 10:28:19 +0200

    Commit final

---
+++ 29_proyecto_formacion/src/model/Alumno.java
@@ -0,0 +1,13 @@
+package model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+public class Alumno {
+	private String nombre;
+	private double nota;
+}
